d0ff4f3bf7376ddbd32eea8c15fe81c9904415f9
Borroot <borroot@live.nl>
2021-08-23 22:57:03 +0200

    Remove unused variable

--- src/constants.asm
+++ src/constants.asm
@@ -4,7 +4,6 @@
 %define cells_x r15w
 %define cells_o r14w
 
-%define winner r11b  ; 0 if none, 1 if x, 2 if o, 4 if draw
 %define input r12b   ; move that was inputted
 %define onturn r13b  ; x if 0, o if 1
 
